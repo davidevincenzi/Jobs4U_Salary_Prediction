824888e6a29ae45563d7045113cf47fae3e08eaa
Davide <davide@vincenzi.de>
2022-06-14 13:17:40 +0200

    Updated contacts page

--- contacts_page.py
+++ contacts_page.py
@@ -1,10 +1,8 @@
 import streamlit as st
 
-@st.cache
-
 def show_contacts_page():
     st.title("Developed by Davide Vincenzi")
 
-    st.write("""Student ID: #010054956""")
+    st.write("Student ID: #010054956")
     
 
