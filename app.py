0e919c24b89c4fff3ae125c288732c7ce1d6de08
Davide <davide@vincenzi.de>
2022-06-14 12:56:11 +0200

    Added contacts page

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from streamlit_option_menu import option_menu
 
 from prediction_page import show_prediction_page
 from dashboard_page import show_dashboard_page
+from contacts_page import show_contacts_page
 
 
 page = option_menu(
 	menu_title = "Jobs4U",
-	options = ["Prediction", "Dashboard", "Contact"],
+	options = ["Prediction", "Dashboard", "Contacts"],
 	icons = ["coin", "speedometer", "envelope"],
 	default_index = 0,
 	)
 
 if page == "Prediction":
     show_prediction_page()
+else if page == "Contacts":
+	show_contacts_page()
 else:
-    show_dashboard_page()
+	show_dashboard_page()
---
+++ contacts_page.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+@st.cache
+
+def show_contacts_page():
+    st.title("Developed by Davide Vincenzi")
+
+    st.write("""Student ID: #010054956""")
+    
+
